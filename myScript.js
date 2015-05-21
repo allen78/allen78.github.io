a1571bcb6063c43109087740fd5c83c6d9057456
Allen <allenchen1988@gmail.com>
2015-05-20 22:03:51 -0700

    Add some AngularJS functions in projects

--- myScript.js
+++ myScript.js
@@ -1,3 +1,18 @@
+(function(){
+	var app = angular.module('portfolio',[]);
+
+	app.controller('ProjectController',function(){
+		this.tab = 1;
+		this.products = projects;
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+})();
+
 $(document).ready(function(){
 	$("#photo").mouseenter(function(){
 		$(this).attr("src","img/Allen.jpg");
